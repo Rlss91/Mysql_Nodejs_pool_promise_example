36382900615e8814609d1df4a3d0b1617a68c3f7
rlss91 <rlss91@gmail.com>
2020-07-02 21:29:45 +0300

    selectAll

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  let data = await clients.selectClient(req.query.id);
-  // console.log(data[0][0].name);
-  res.render("index", { title: "Express", data: data[0] });
+  // let data = await clients.selectClient(req.query.id);
+  try {
+    let data = await clients.selectAll();
+    // console.log(data[0][0].name);
+    res.render("index", { title: "Express", data: data[0] });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
